5ed2ceff8976e14c650023a05b8a8b320962ff05
Louie Rosero <rosero.louie@yahoo.com>
2017-08-02 06:22:38 +0800

    Commit 4 - Search Bar Animation
    Updates of Search Bar animation in CSS and JS.

--- js/script.js
+++ js/script.js
@@ -3,5 +3,17 @@ $(function() {
 	var searchField = $('#query');
 	var icon = $('#search-btn');
 	
-	// Focus Handler
+	// Focus Event Handler
+	$(searchField).on('focus', function() {
+		$(this).animate({	width: '90%'	}, 400);
+		$(icon).animate({ right: '-8px' }, 400);
+	});
+	
+	// Blur Event Handler
+	$(searchField).on('blur', function() {
+		if (searchField.val() == '') {
+			$(searchField).animate({ width: '45%' }, 400, function() {});
+			$(icon).animate({ right: '364px' }, 400, function() {});
+		}
+	});
 });
