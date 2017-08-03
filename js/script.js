854ce8afea68d6d7138bce568159479c8f88c893
Louie Rosero <rosero.louie@yahoo.com>
2017-08-03 22:03:46 +0800

    Commit 6 - Displaying Results from API
    Displaying Youtube Video information from Youtube API and creating the
    'Previous' and 'Next' button for Pagination.

--- js/script.js
+++ js/script.js
@@ -51,6 +51,54 @@ function search() {
 					// Display Results
 					$('#results').append(output);
 				});
+				
+				var buttons = getButtons(prevPageToken, nextPageToken);
+				
+				// Display Buttons
+				$('#buttons').append(buttons);
 			}
 	);
+}
+
+// Build Output
+function getOutput(item) {
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+	
+	// Build Output String
+	var output = '<li>' +
+	'<div class="list-left">' +
+	'<img src="'+thumb+'">' +
+	'</div>' +
+	'<div class="list-right">' +
+	'<h3>'+title+'</h3>' + 
+	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
+	'<p>'+description+'</p>' +
+	'</div>' +
+	'</li>' +
+	'<div class="clearfix"></div>' +
+	'';
+	
+	return output;
+}
+
+// Build the Buttons
+function getButtons(prevPageToken, nextPageToken) {
+	if (!prevPageToken) {
+		var btnoutput = '<div class="button-container">' +
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'" ' +
+		'onclick="nextPageToken();">Next Page</button></div>';
+	} else {
+		var btnoutput = '<div class="button-container">' +
+		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'" ' +
+		'onclick="prevPage();">Previous Page</button>' +
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'" ' +
+		'onclick="nextPage();">Next Page</button></div>';
+	}
+	
+	return btnoutput;
 }
