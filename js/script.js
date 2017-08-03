ee0f4ba5b8be3a86f8622cd399b6822b094331ea
Louie Rosero <rosero.louie@yahoo.com>
2017-08-03 23:28:08 +0800

    Commit 7 - Paging Buttons and Lightbox
    Using Fancybox for displaying the Youtube Video.
    Creating Function for Previous and Next Buttons.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// Calling the Fancy Box Plug-in
+$(document).ready(function() {
+$('.fancybox').fancybox();
+});
+
 // Searchbar Handler
 $(function() {
 	var searchField = $('#query');
@@ -60,6 +65,92 @@ function search() {
 	);
 }
 
+// Previous Page Function
+function prevPage() {
+	var token = $('#prev-button').data('token');
+	var q = $('#prev-button').data('query');
+	
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Inputs
+	q = $('#query').val();
+	
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search", {
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type: 'video',
+			key: 'AIzaSyDIZB_3qSejx0vh0ec5uK2rf9740CY0G90' },
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Log Data
+				console.log(data);
+				
+				$.each(data.items, function(i, item) {
+					// Get Output
+					var output = getOutput(item);
+					
+					// Display Results
+					$('#results').append(output);
+				});
+				
+				var buttons = getButtons(prevPageToken, nextPageToken);
+				
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
+// Next Page Function
+function nextPage() {
+	var token = $('#next-button').data('token');
+	var q = $('#next-button').data('query');
+	
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Inputs
+	q = $('#query').val();
+	
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search", {
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type: 'video',
+			key: 'AIzaSyDIZB_3qSejx0vh0ec5uK2rf9740CY0G90' },
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Log Data
+				console.log(data);
+				
+				$.each(data.items, function(i, item) {
+					// Get Output
+					var output = getOutput(item);
+					
+					// Display Results
+					$('#results').append(output);
+				});
+				
+				var buttons = getButtons(prevPageToken, nextPageToken);
+				
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
 // Build Output
 function getOutput(item) {
 	var videoId = item.id.videoId;
@@ -75,7 +166,7 @@ function getOutput(item) {
 	'<img src="'+thumb+'">' +
 	'</div>' +
 	'<div class="list-right">' +
-	'<h3>'+title+'</h3>' + 
+	'<h3><a href="http://www.youtube.com/embed/'+videoId+'" data-fancybox data-type="iframe" >'+title+'</a></h3>' + 
 	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
 	'<p>'+description+'</p>' +
 	'</div>' +
@@ -91,7 +182,7 @@ function getButtons(prevPageToken, nextPageToken) {
 	if (!prevPageToken) {
 		var btnoutput = '<div class="button-container">' +
 		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'" ' +
-		'onclick="nextPageToken();">Next Page</button></div>';
+		'onclick="nextPage();">Next Page</button></div>';
 	} else {
 		var btnoutput = '<div class="button-container">' +
 		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'" ' +
